16ad7eeb9624a13d199f8646086850820174ad0e
lanthuong <mtischer53@gmail.com>
2015-07-14 18:13:36 +0700

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'dungnv',
+    'password' => 'dungnv',
+    'charset' => 'utf8',
+];
